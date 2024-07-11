220bde3b81c8e45e9a547b69ccb86288fbbc0829
marco-vb <marcofilipe03@hotmail.com>
2024-07-11 16:10:48 +0100

    Added config parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // dbg!(args);
+
+    let config = parse_args(&args);
+
+    println!("Searching for {} in {}.", config.expression, config.file);
+
+    let contents = fs::read_to_string(config.file).expect("Could not read from specified file");
+
+    println!("File contents:\n{contents}");
+}
+
+struct Config<'a> {
+    expression: &'a String,
+    file: &'a String,
+}
+
+fn parse_args(args: &Vec<String>) -> Config {
+    if args.len() != 3 {
+        panic!("usage: minigrep <expression> <file_path>");
+    }
+
+    Config { expression: &args[1], file: &args[2] }
 }
